3d6be550ca87db209f46a57c78d94540371f048b
NainiKhan <hasnainashfaq4411@gmail.com>
2024-07-06 08:46:10 +0100

    thread

--- lib/appScreens/Home.dart
+++ lib/appScreens/Home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:thread/appScreens/favoriteScreen.dart';
+import 'package:thread/appScreens/feedScreen.dart';
+import 'package:thread/appScreens/postScreen.dart';
+import 'package:thread/appScreens/profileScreen.dart';
+import 'package:thread/appScreens/searchscreen.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,21 +13,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    int selectedIndex = 0;
+      int selectedIndex = 0;
     // We create this _pages and selectedIndex because we are using Bottom Navigation bar so we will show the
     // Selected page when someone will click on the bottom navigation bar icons
     // This _pages List will contain Widget
-    List<Widget> _pages = [
-        const Center(child: Text("Home"),),
-        const Center(child: Text("Feed"),),
-        const Center(child: Text("Post"),),
-        const Center(child: Text("Favorite"),),
-        const Center(child: Text("Profile"),),
+    /*
+    So when we will show the actual data so we will create another class of widgets like we will create different
+    class for Feed, Post, Favorite so these widget class will be saved in this list and on click of bottom
+    navigation bar we will call these classes
+     */
+    final List<Widget>  pages = [
+        
+        const FeedScreen(),
+        const SearchScreen(),
+        const PostScreen(),
+        const FavoriteScreen(),
+        const ProfileScreen(),
     ];
+  @override
+  Widget build(BuildContext context) {
+
     return Scaffold(
-      body: _pages[selectedIndex],
+      // Now in body we are telling to scaffold that show the Widgets from pages List
+      // And show us the specific widget from the index (Index is saved in Selected index as per bottom navigation bar)
+      // so when the index is 3 it shows Post 
+      
+      body: pages[selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         // Selected Index Mean which _list view we are showing or which Icon is selected by user
         // BY default we set it to Home but when user will select search, messages icons from bottom navigation
@@ -37,13 +53,15 @@ class _HomeState extends State<Home> {
           // By Default Bottom Navigation bar has index according to the Items that we created so in this case
           // we created 5 items so index will 5 so we save the  bottom navigation index to selected
           // So once user will click on 5th index profile selected index value will be change to 5
-          selectedIndex = index;
+          setState(() {
+            selectedIndex = index;
+          });
         },
         // Type is fixed it means that when we will scroll in list so it will always remain fixed like thread, Insta
-        
+
         type: BottomNavigationBarType.fixed,
         items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Feed"),
         BottomNavigationBarItem(icon: Icon(Icons.search), label: "search"),
         BottomNavigationBarItem(icon: Icon(Icons.add), label: "Post"),
         BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorite"),
